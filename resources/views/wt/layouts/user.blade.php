e115d2078a4de2f6d6355d9aea63f5553a4cb404
AlepTeoh <teohalif83@gmail.com>
2026-06-30 12:08:11 +0800

    wt

--- resources/views/wt/layouts/user.blade.php
+++ resources/views/wt/layouts/user.blade.php
@@ -29,7 +29,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
 <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
-<link href="{{ asset('assets/css/wtsystem.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/wtsystem.css') }}?v={{ time() }}" rel="stylesheet">
 <style>
 /* wtsystem.css is the single source of truth */
 </style>
