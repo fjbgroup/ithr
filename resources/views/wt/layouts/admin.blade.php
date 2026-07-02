5d09ba3f9ed6ac8fe596dd2ed21dd7cbe1fcefa2
AlepTeoh <teohalif83@gmail.com>
2026-07-02 11:24:33 +0800

    refresh 30

--- resources/views/wt/layouts/admin.blade.php
+++ resources/views/wt/layouts/admin.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv="refresh" content="5">
+    <meta http-equiv="refresh" content="30">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
